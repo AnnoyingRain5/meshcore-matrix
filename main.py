76707d8ca998b568473d4133ec49b9c02d78448c
AnnoyingRains <annoyingrain5@gmail.com>
2026-08-15 16:35:40 +1000

    add AGPL licence

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE Version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+#
+# You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 import asyncio
 import hashlib
 import time
